3e2f35d2da866dce975c690c3394c7c1631d995d
hanselcarter <barahonahansel@hotmail.com>
2023-05-31 15:22:38 -0600

    chore: beutifying

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2>Home</h2>
+    <p>lorem ipsum</p>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="css" scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+p {
+  margin: 20px 0;
+}
+</style>
---
+++ pages/products/[id].vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <p>product details {{ id }}</p>
+  </div>
+</template>
+
+<script setup>
+definePageMeta({
+  layout: "products",
+});
+const { id } = useRoute().params;
+</script>
+
+<style lang="css" scoped></style>
